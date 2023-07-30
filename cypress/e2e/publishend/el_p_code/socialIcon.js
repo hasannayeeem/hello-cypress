2193dda0b7757487d87a42f3b4c7d254b804ffdc
hasannayeeem <hasannayeeem@gmail.com>
2023-07-30 12:04:29 +0600

    added social icon test

--- cypress/e2e/publishend/el_p_code/socialIcon.js
+++ cypress/e2e/publishend/el_p_code/socialIcon.js
@@ -1,18 +1,42 @@
 const socialIcon = () => {
     // start to checking SOCIAL ICON
-    cy.get(`ul`)
-    // cy.checkCssProperties(".dorik-list", {
-    //     "flex-direction": "column",
-    //     "align-items": "center",
-    // });
-
-    // cy.wait(200)
-    // cy.get(".dorik-list--link:eq(1)")
-    // .trigger("mouseover")
-    // .should("have.css", {
-    //   "color": "rgb(161, 69, 161)",
-    //   "text-decoration": "overline rgb(161, 69, 161)"
-    // })
+    cy.get(`[class^="dorik-socialIcon-"] > li svg`)
+    cy.checkCssProperties(`[class^="dorik-socialIcon-"]`, {
+      "display": "flex",
+      "flex-direction": "column",
+      "line-height": "16px",
+      "margin-top": "0px",
+      "margin-bottom": "0px",
+      "margin-left": "0px",
+      "margin-right": "0px",
+      "padding-top": "0px",
+      "padding-bottom": "0px",
+      "padding-left": "0px",
+      "padding-right": "0px",
+      "justify-content": "flex-start",
+      "align-items": "center",
+      "background-color": "rgb(161, 69, 161)"
+    });
+    cy.get(`[class^="dorik-socialIcon-"]`)
+    .trigger("mouseover")
+    .should("have.css", {
+      "background-color": "rgb(170, 17, 85)"
+    })
+    cy.checkCssProperties(`[class^="dorik-socialIcon-"] > li`, {
+      "margin-bottom": "14px"
+    });
+    cy.checkCssProperties(`[class^="dorik-socialIcon-"] > li svg`, {
+          "fill": "rgb(27, 25, 25)",
+          "height": "40px",
+          "width": "40px",
+          "min-width": "40px"
+      });
+    cy.get(`[class^="dorik-socialIcon-"] > li a`).last().should("have.attr", "href", "/check-button")
+    cy.get(`[class^="dorik-socialIcon-"] > li svg`).last()
+    .trigger("mouseover")
+    .should("have.css", {
+      "background-color": "rgb(170, 17, 85)"
+    })
   };
   export default socialIcon
   
--- cypress/e2e/publishend/elements/socialIcon.cy.js
+++ cypress/e2e/publishend/elements/socialIcon.cy.js
@@ -1,22 +1,11 @@
 import social_Icon from "../el_p_code/socialIcon";
 
 export const socialIcon = () => {
-    it("test SOCIAL ICON on publishend", () => {
-        cy.getOrCreateToken();
-        cy.visit(`${Cypress.env("publishedURL")}`);
-
+  it("test SOCIAL ICON on publishend", () => {
+    cy.getOrCreateToken();
+    cy.visit(`${Cypress.env("publishedURL")}`);
     // start to checking SOCIAL ICON
-    social_Icon()
-    // cy.checkCssProperties(".selector", {
-    // })
-    
-    // cy.wait(200)
-    // cy.get(".dorik-list--link:eq(1)")
-    // .trigger("mouseover")
-    // .should("have.css", {
-    //   "color": "rgb(161, 69, 161)",
-    //   "text-decoration": "overline rgb(161, 69, 161)"
-    // })
-})
+    social_Icon();
+  });
 };
-socialIcon()
+socialIcon();
